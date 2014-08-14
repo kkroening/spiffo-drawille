97b8575854def5128c2da2d2ecc27854b4dabfaf
Karl Kroening <karlk@kralnet.us>
2014-08-14 01:02:39 -0500

    Update to latest Drawille

--- drawille.py
+++ drawille.py
@@ -15,9 +15,12 @@
 #
 # (C) 2014- by Adam Tauber, <asciimoo@gmail.com>
 
-from collections import defaultdict
 import math
+import os
 from sys import version_info
+from collections import defaultdict
+from time import sleep
+import curses
 
 IS_PY3 = version_info[0] == 3
 
@@ -50,7 +53,6 @@ braille_char_offset = 0x2800
 def getTerminalSize():
     """Returns terminal width, height
     """
-    import os
     env = os.environ
 
     def ioctl_GWINSZ(fd):
@@ -82,33 +84,34 @@ def normalize(coord):
 
     if coord_type == int:
         return coord
-
     elif coord_type == float:
         return int(round(coord))
+    else:
+        raise TypeError("Unsupported coordinate type <{0}>".format(type(coord)))
 
 
 def intdefaultdict():
     return defaultdict(int)
 
 
+def get_pos(x, y):
+    """Convert x, y to cols, rows"""
+    return normalize(x) // 2, normalize(y) // 4
+
+
 class Canvas(object):
-    """This class implements the pixel surface """
-    def __init__(self, line_ending='\n'):
+    """This class implements the pixel surface."""
+
+    def __init__(self, line_ending=os.linesep):
         super(Canvas, self).__init__()
         self.clear()
         self.line_ending = line_ending
+
 
     def clear(self):
         """Remove all pixels from the :class:`Canvas` object."""
         self.chars = defaultdict(intdefaultdict)
 
-    def _get_pos(self, x, y):
-        """Convert x, y to cols, rows"""
-        x = normalize(x)
-        y = normalize(y)
-        cols = x // 2
-        rows = y // 4
-        return x, y, cols, rows
 
     def set(self, x, y):
         """Set a pixel of the :class:`Canvas` object.
@@ -116,10 +119,15 @@ class Canvas(object):
         :param x: x coordinate of the pixel
         :param y: y coordinate of the pixel
         """
-        x, y, px, py = self._get_pos(x, y)
-        if type(self.chars[py][px]) != int:
+        x = normalize(x)
+        y = normalize(y)
+        col, row = get_pos(x, y)
+
+        if type(self.chars[row][col]) != int:
             return
-        self.chars[py][px] |= pixel_map[y % 4][x % 2]
+
+        self.chars[row][col] |= pixel_map[y % 4][x % 2]
+
 
     def unset(self, x, y):
         """Unset a pixel of the :class:`Canvas` object.
@@ -127,13 +135,19 @@ class Canvas(object):
         :param x: x coordinate of the pixel
         :param y: y coordinate of the pixel
         """
-        x, y, px, py = self._get_pos(x, y)
-        if type(self.chars[py][px]) == int:
-            self.chars[py][px] &= ~pixel_map[y % 4][x % 2]
-        if type(self.chars[py][px]) != int or self.chars[py][px] == 0:
-            del(self.chars[py][px])
-        if not self.chars.get(py):
-            del(self.chars[py])
+        x = normalize(x)
+        y = normalize(y)
+        col, row = get_pos(x, y)
+
+        if type(self.chars[row][col]) == int:
+            self.chars[row][col] &= ~pixel_map[y % 4][x % 2]
+
+        if type(self.chars[row][col]) != int or self.chars[row][col] == 0:
+            del(self.chars[row][col])
+
+        if not self.chars.get(row):
+            del(self.chars[row])
+
 
     def toggle(self, x, y):
         """Toggle a pixel of the :class:`Canvas` object.
@@ -141,11 +155,15 @@ class Canvas(object):
         :param x: x coordinate of the pixel
         :param y: y coordinate of the pixel
         """
-        x, y, px, py = self._get_pos(x, y)
-        if type(self.chars[py][px]) != int or self.chars[py][px] & pixel_map[y % 4][x % 2]:
+        x = normalize(x)
+        y = normalize(y)
+        col, row = get_pos(x, y)
+
+        if type(self.chars[row][col]) != int or self.chars[row][col] & pixel_map[y % 4][x % 2]:
             self.unset(x, y)
         else:
             self.set(x, y)
+
 
     def set_text(self, x, y, text):
         """Set text to the given coords.
@@ -153,10 +171,11 @@ class Canvas(object):
         :param x: x coordinate of the text start position
         :param y: y coordinate of the text start position
         """
-        x = normalize(x / 2)
-        y = normalize(y / 4)
+        col, row = get_pos(x, y)
+
         for i,c in enumerate(text):
-            self.chars[y][x+i] = c
+            self.chars[row][col+i] = c
+
 
     def get(self, x, y):
         """Get the state of a pixel. Returns bool.
@@ -164,10 +183,11 @@ class Canvas(object):
         :param x: x coordinate of the pixel
         :param y: y coordinate of the pixel
         """
+        x = normalize(x)
+        y = normalize(y)
         dot_index = pixel_map[y % 4][x % 2]
-        x = normalize(x / 2)
-        y = normalize(y / 4)
-        char = self.chars.get(y, {}).get(x)
+        col, row = get_pos(x, y)
+        char = self.chars.get(row, {}).get(col)
 
         if not char:
             return False
@@ -176,6 +196,7 @@ class Canvas(object):
             return True
 
         return bool(char & dot_index)
+
 
     def rows(self, min_x=None, min_y=None, max_x=None, max_y=None):
         """Returns a list of the current :class:`Canvas` object lines.
@@ -217,6 +238,7 @@ class Canvas(object):
 
         return ret
 
+
     def frame(self, min_x=None, min_y=None, max_x=None, max_y=None):
         """String representation of the current :class:`Canvas` object pixels.
 
@@ -234,6 +256,13 @@ class Canvas(object):
 
 
 def line(x1, y1, x2, y2):
+    """Returns the coords of the line between (x1, y1), (x2, y2)
+
+    :param x1: x coordinate of the startpoint
+    :param y1: y coordinate of the startpoint
+    :param x2: x coordinate of the endpoint
+    :param y2: y coordinate of the endpoint
+    """
 
     x1 = normalize(x1)
     y1 = normalize(y1)
@@ -252,23 +281,137 @@ def line(x1, y1, x2, y2):
         y = y1
 
         if ydiff:
-            y += (float(i)*ydiff)/r*ydir
+            y += (float(i) * ydiff) / r * ydir
         if xdiff:
-            x += (float(i)*xdiff)/r*xdir
+            x += (float(i) * xdiff) / r * xdir
 
         yield (x, y)
 
 
 def polygon(center_x=0, center_y=0, sides=4, radius=4):
-    degree = float(360)/sides
+    degree = float(360) / sides
 
     for n in range(sides):
-        a = n*degree
-        b = (n+1)*degree
-        x1 = (center_x+math.cos(math.radians(a)))*(radius+1)/2
-        y1 = (center_x+math.sin(math.radians(a)))*(radius+1)/2
-        x2 = (center_x+math.cos(math.radians(b)))*(radius+1)/2
-        y2 = (center_x+math.sin(math.radians(b)))*(radius+1)/2
+        a = n * degree
+        b = (n + 1) * degree
+        x1 = (center_x + math.cos(math.radians(a))) * (radius + 1) / 2
+        y1 = (center_y + math.sin(math.radians(a))) * (radius + 1) / 2
+        x2 = (center_x + math.cos(math.radians(b))) * (radius + 1) / 2
+        y2 = (center_y + math.sin(math.radians(b))) * (radius + 1) / 2
 
-        for x,y in line(x1,y1,x2,y2):
-            yield x,y
+        for x, y in line(x1, y1, x2, y2):
+            yield x, y
+
+
+class Turtle(Canvas):
+    """Turtle graphics interface
+    http://en.wikipedia.org/wiki/Turtle_graphics
+    """
+
+    def __init__(self, pos_x=0, pos_y=0):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.rotation = 0
+        self.brush_on = True
+        super(Turtle, self).__init__()
+
+
+    def up(self):
+        """Pull the brush up."""
+        self.brush_on = False
+
+
+    def down(self):
+        """Push the brush down."""
+        self.brush_on = True
+
+
+    def forward(self, step):
+        """Move the turtle forward.
+
+        :param step: Integer. Distance to move forward.
+        """
+        x = self.pos_x + math.cos(math.radians(self.rotation)) * step
+        y = self.pos_y + math.sin(math.radians(self.rotation)) * step
+        prev_brush_state = self.brush_on
+        self.brush_on = True
+        self.move(x, y)
+        self.brush_on = prev_brush_state
+
+
+    def move(self, x, y):
+        """Move the turtle to a coordinate.
+
+        :param x: x coordinate
+        :param y: y coordinate
+        """
+        if self.brush_on:
+            for lx, ly in line(self.pos_x, self.pos_y, x, y):
+                self.set(lx, ly)
+
+        self.pos_x = x
+        self.pos_y = y
+
+
+    def right(self, angle):
+        """Rotate the turtle (positive direction).
+
+        :param angle: Integer. Rotation angle in degrees.
+        """
+        self.rotation += angle
+
+
+    def left(self, angle):
+        """Rotate the turtle (negative direction).
+
+        :param angle: Integer. Rotation angle in degrees.
+        """
+        self.rotation -= angle
+
+
+    def back(self, step):
+        """Move the turtle backwards.
+
+        :param step: Integer. Distance to move backwards.
+        """
+        self.forward(-step)
+
+
+    # aliases
+    pu = up
+    pd = down
+    fd = forward
+    mv = move
+    rt = right
+    lt = left
+    bk = back
+
+
+def animate(canvas, fn, delay=1./24, *args, **kwargs):
+    """Animation automatition function
+
+    :param canvas: :class:`Canvas` object
+    :param fn: Callable. Frame coord generator
+    :param delay: Float. Delay between frames.
+    :param *args, **kwargs: optional fn parameters
+    """
+
+    # python2 unicode curses fix
+    if not IS_PY3:
+        import locale
+        locale.setlocale(locale.LC_ALL, "")
+
+    def animation(stdscr):
+
+        for frame in fn(*args, **kwargs):
+            for x,y in frame:
+                canvas.set(x,y)
+
+            f = canvas.frame()
+            stdscr.addstr(0, 0, '{0}\n'.format(f))
+            stdscr.refresh()
+            if delay:
+                sleep(delay)
+            canvas.clear()
+
+    curses.wrapper(animation)
